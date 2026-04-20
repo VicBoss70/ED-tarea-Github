e06770bea17fdf018bf6061283ddd5dac7cbdadd
Héctor García <saschagarcia07@gmail.com>
2026-04-20 16:25:34 +0200

    Update Hola.java

--- Hola.java
+++ Hola.java
@@ -3,3 +3,4 @@ public class Hola {
         System.out.println("Hola, este es mi primer archivo en la rama desarrollo");
     }
 }
+// Cambio realizado desde la interfaz web de GitHub para probar el Pull.
